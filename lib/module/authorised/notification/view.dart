5af3edf9b3e27a3e444765bd39096be0f0c7aa70
Snehae15 <snehae15@gmail.com>
2025-05-07 08:57:57 +0530

    .

--- lib/module/authorised/notification/view.dart
+++ lib/module/authorised/notification/view.dart
@@ -1,39 +1,104 @@
-// import 'package:ecommerce/module/authorised/notification/widget/notification_tile.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-// class NotificationPage extends StatelessWidget {
-//   final controller = Get.put(NotificationController());
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
 
-//   NotificationPage({super.key});
+  final notifications = const [
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Notifications'),
-//         backgroundColor: Colors.white,
-//         foregroundColor: Colors.black,
-//         elevation: 0,
-//       ),
-//       backgroundColor: Colors.grey.shade100,
-//       body: Obx(() {
-//         final items = controller.notifications;
-//         return ListView.separated(
-//           itemCount: items.length,
-//           separatorBuilder: (_, __) => Container(
-//             height: 1,
-//             color: Colors.grey.shade200,
-//           ),
-//           itemBuilder: (context, index) {
-//             final item = items[index];
-//             return NotificationTile(
-//               data: item,
-//             );
-//           },
-//         );
-//       }),
-//     );
-//   }
-// }
+    {
+      'iconText': 'Nr',
+      'title': 'New order',
+      'subtitle': 'ORD 001',
+      'color': Colors.orange,
+    },
+    {
+      'iconText': 'Nr',
+      'title': 'New order',
+      'subtitle': 'ORD 001',
+      'color': Colors.orange,
+    },
+    {
+      'iconText': 'Nr',
+      'title': 'New order',
+      'subtitle': 'ORD 001',
+      'color': Colors.orange,
+    },
+    {
+      'iconText': 'Nr',
+      'title': 'New order',
+      'subtitle': 'ORD 001',
+      'color': Colors.orange,
+    },
+
+   
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        
+        title: Text('Notifications'),
+        
+        // leading: IconButton(
+        //   icon: SvgPicture.asset(.arrowLeft1),
+        //   onPressed: () {
+        //     Navigator.pop(context);
+        //   },
+        // ),
+        // actions: [
+        //   TextButton.icon(
+        //     onPressed: () {},
+        //     icon: const Icon(Icons.remove_red_eye_outlined, color: Colors.orange),
+        //     label: const Text(
+        //       'Mark as read all',
+        //       style: TextStyle(color: Colors.orange),
+        //     ),
+        //   ),
+        // ],
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+
+          final item = notifications[index];
+          
+          
+          final backgroundColor = index % 2 == 0 ? Colors.white : Colors.grey[200];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              tileColor: backgroundColor,
+              leading: CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.transparent,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    item['iconText'].toString()!,
+                    style: TextStyle(
+                      color: item['color'] as Color,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                foregroundColor: item['color'] as Color,
+              ),
+              title: Text(
+                item['title'].toString()!,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(item['subtitle'].toString()!),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/widget/cutom_appbar/view.dart
+++ lib/widget/cutom_appbar/view.dart
@@ -1,4 +1,3 @@
-
 import 'package:ecommerce/gen/assets.gen.dart';
 import 'package:ecommerce/module/authorised/bottom_navbar/bottom_navbar.dart';
 import 'package:ecommerce/module/authorised/dashboard/controller.dart';
@@ -8,133 +7,20 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 
-// class CustomAppBar extends StatelessWidget{
-//   final TextEditingController? controller;
-//   final String hintText;
-//   final VoidCallback? onCartTap;
-//   final VoidCallback? onClearSearch;
-//   final RxString? imageUrl;
-
-//   const CustomAppBar(
-//       {super.key,
-//       this.controller,
-//       this.hintText = "Search",
-//       this.onCartTap,
-//       this.onClearSearch,
-//       this.imageUrl});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.fromLTRB(16, 30, 16, 16),
-//       color: const Color(0xFFF5F5F5),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           // Cart Icon aligned right
-//           Row(
-//             // mainAxisAlignment: MainAxisAlignment.end,
-//             children: [
-//               ClipRRect(
-//                 borderRadius: BorderRadius.circular(40),
-//                 child:
-//                     Assets.images.company.companyLogo.image(fit: BoxFit.cover),
-//               ),
-//               Spacer(),
-//               Obx(() => CircleAvatar(
-//                     maxRadius: 20, // Adjust radius as needed
-//                     backgroundColor: const Color.fromARGB(255, 166, 164, 164),
-//                     child: ClipRRect(
-//                       borderRadius: BorderRadius.circular(40),
-//                       child: Image.network(
-//                         formatImageUrl(imageUrl?.value),
-//                         fit: BoxFit.cover,
-//                         width: 80, // match the maxRadius * 2
-//                         height: 80,
-//                         errorBuilder: (context, error, stackTrace) => Icon(
-//                           Icons.image,
-//                           size: 20,
-//                           color: const Color.fromARGB(255, 78, 77, 77),
-//                         ),
-//                       ),
-//                     ),
-//                   )),
-//               Gap(10),
-//               GestureDetector(
-//                 onTap: onCartTap,
-//                 child: Container(
-//                   width: 50,
-//                   height: 50,
-//                   decoration: const BoxDecoration(
-//                     color: Colors.white,
-//                     shape: BoxShape.circle,
-//                   ),
-//                   child: const Icon(Icons.shopping_cart_outlined,
-//                       color: Colors.orange),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           const SizedBox(height: 16),
-//           // Search bar
-//           Container(
-//             height: 50,
-//             decoration: BoxDecoration(
-//               color: Colors.white,
-//               borderRadius: BorderRadius.circular(30),
-//               boxShadow: const [
-//                 BoxShadow(
-//                   color: Colors.black12,
-//                   blurRadius: 4,
-//                   offset: Offset(0, 2),
-//                 ),
-//               ],
-//             ),
-//             child: TextField(
-//               controller: controller,
-//               decoration: InputDecoration(
-//                 isDense: true,
-//                 hintText: hintText,
-//                 hintStyle: const TextStyle(color: Colors.grey),
-//                 prefixIcon: const Icon(Icons.search, color: Colors.black54),
-//                 suffixIcon: GestureDetector(
-//                   onTap: onClearSearch,
-//                   child: const Icon(Icons.close, color: Colors.black54),
-//                 ),
-//                 border: InputBorder.none,
-//                 contentPadding:
-//                     const EdgeInsets.symmetric(vertical: 14, horizontal: 0),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 class CustomAppBar<T> extends StatelessWidget {
-  // final TextEditingController? controller;
   final String hintText;
-  final VoidCallback? onCartTap;
+  final VoidCallback? onNotificationTap;
   final VoidCallback? onClearSearch;
   final RxString? imageUrl;
   final DashboardController controller;
-
-// final FutureOr<Iterable<T>> Function(String value) optionsBuilder;
-//   final Widget Function(T option) autoCompleteTileBuilder;
-//     final String Function(T option) displayStringForOption;
-//     final Widget Function(T value) selectedBuilder;
-//     final Function(T)? onSelect;
 
   const CustomAppBar({
     super.key,
     required this.controller,
     this.hintText = "Search",
-    this.onCartTap,
+    this.onNotificationTap,
     this.onClearSearch,
     this.imageUrl,
-    //  required this.optionsBuilder, required this.autoCompleteTileBuilder, required this.displayStringForOption, required this.selectedBuilder, this.onSelect,
   });
 
   @override
@@ -163,37 +49,22 @@ class CustomAppBar<T> extends StatelessWidget {
                 ),
               ),
               const Spacer(),
-              // Obx(() => CircleAvatar(
-              //       maxRadius: 20,
-              //       backgroundColor: const Color.fromARGB(255, 166, 164, 164),
-              //       child: ClipRRect(
-              //         borderRadius: BorderRadius.circular(40),
-              //         child: Image.network(
-              //           formatImageUrl(imageUrl?.value),
-              //           fit: BoxFit.cover,
-              //           width: 80,
-              //           height: 80,
-              //           errorBuilder: (context, error, stackTrace) =>
-              //               const Icon(
-              //             Icons.image,
-              //             size: 20,
-              //             color: Color.fromARGB(255, 78, 77, 77),
-              //           ),
-              //         ),
-              //       ),
-              //     )),
+
               const Gap(10),
               GestureDetector(
-                onTap: onCartTap,
+                onTap: onNotificationTap,
                 child: Container(
-                  width: 50,
-                  height: 50,
+                  width: 70,
+                  height: 70,
                   decoration: const BoxDecoration(
                     color: Colors.white,
                     shape: BoxShape.circle,
                   ),
-                  child: const Icon(Icons.shopping_cart_outlined,
-                      color: Colors.orange),
+                  child: const Icon(
+                    Icons.circle_notifications_outlined,
+                    color: Colors.orange,
+                    size: 30, // Increase this as needed
+                  ),
                 ),
               ),
             ],
@@ -201,44 +72,11 @@ class CustomAppBar<T> extends StatelessWidget {
 
           const SizedBox(height: 16),
 
-          // Search bar
-          // Container(
-          //   height: 50,
-          //   decoration: BoxDecoration(
-          //     color: Colors.white,
-          //     borderRadius: BorderRadius.circular(30),
-          //     boxShadow: const [
-          //       BoxShadow(
-          //         color: Colors.black12,
-          //         blurRadius: 4,
-          //         offset: Offset(0, 2),
-          //       ),
-          //     ],
-          //   ),
-          //   child: TextField(
-          //     controller: controller,
-          //     decoration: InputDecoration(
-          //       isDense: true,
-          //       hintText: hintText,
-          //       hintStyle: const TextStyle(color: Colors.grey),
-          //       prefixIcon: const Icon(Icons.search, color: Colors.black54),
-          //       suffixIcon: GestureDetector(
-          //         onTap: onClearSearch,
-          //         child: const Icon(Icons.close, color: Colors.black54),
-          //       ),
-          //       border: InputBorder.none,
-          //       contentPadding:
-          //           const EdgeInsets.symmetric(vertical: 14, horizontal: 0),
-          //     ),
-          //   ),
-          // ),
-
           AutocompleteWidget(
             autoCompleteTileBuilder: (CategoryModel option) =>
                 Text(option.categoryName),
             displayStringForOption: (CategoryModel option) =>
                 option.categoryName,
-            // controller: controller.bundleCreateModel[index].bcodeCont,
             initialvalue: '',
             hintText: 'Search category',
             optionsBuilder: (String value) async {
@@ -250,19 +88,18 @@ class CustomAppBar<T> extends StatelessWidget {
                 (cat) => cat.categoryName.toLowerCase().contains(input),
               );
             },
-
-            // maxWidth: 500,
             onSelect: (value) {
               int index = controller.categories.indexOf(value);
-              // controller.index = index;
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => NavScreen(index: 1,id:controller.categories.value[index].id! ,tabIndex: index,),
+                  builder: (context) => NavScreen(
+                    index: 1,
+                    id: controller.categories.value[index].id!,
+                    tabIndex: index,
+                  ),
                 ),
               );
-              // controller.onCategoryContainerTap(
-              //     index: index, id: controller.categories.value[index].id!);
             },
             selectedBuilder: (CategoryModel selected) =>
                 Text(selected.categoryName),
